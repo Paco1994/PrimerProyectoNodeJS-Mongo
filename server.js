20bdd5d4805bbb194ddf357e681828baceb4b57b
Iván Portillo <ivanportillo@me.com>
2014-12-14 15:35:00 -0500

    Corregido CROS

--- server.js
+++ server.js
@@ -18,6 +18,12 @@ app.use(bodyParser.urlencoded({extended: false }));
 app.use(bodyParser.json());
 app.use(methodOverride());
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 
 app.get('/', function(req, res){
   res.send('Se conecta correctamente a localhost en puerto 500 ;)');
